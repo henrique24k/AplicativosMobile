b09f7ea01868cf56c6aa56b6bbdb910368851cf2
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-08-31 18:42:14 -0300

    Add files via upload

---
+++ TabuadaJS/js/script.js
@@ -0,0 +1,6 @@
+for(var i = 1; i < 11 ; i++){
+    document.write("<h3>Tabuada do " + i + "</h3>");
+    for(var j = 1; j < 11 ; j++){
+        document.write(i + " x " + j + " = " + (i * j) + "<br>");
+    }
+}
